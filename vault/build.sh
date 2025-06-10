ed68d560e3c812fc6913d609271568b81a888d96
Giuseppe <djtech.developer@gmail.com>
2025-06-10 19:28:40 +0200

    Added build.sh script

---
+++ vault/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+python3 -m build
+
+# IF YOU ALSO WANT TO INSTALL, RUN:
+# python3 -m pip install dist/pyvault_client-*.whl --force-reinstall
+#
+# REMEMBER TO CLEAN dist/ BEFORE BUILDING UPDATED VERSIONS IF YOU WANT TO INSTALL THEM
